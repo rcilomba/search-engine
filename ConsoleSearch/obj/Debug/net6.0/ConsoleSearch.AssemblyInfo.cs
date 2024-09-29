3e21587efed668f84667411fb8313cccd3edd2a9
rcilomba <rcilomba@yahoo.com>
2024-09-29 21:27:19 +0200

    update

--- ConsoleSearch/obj/Debug/net6.0/ConsoleSearch.AssemblyInfo.cs
+++ ConsoleSearch/obj/Debug/net6.0/ConsoleSearch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleSearch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fadc0136c1176dbd06e8a7a20f2524e856301d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleSearch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleSearch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Indexer/obj/Debug/net6.0/Indexer.AssemblyInfo.cs
+++ Indexer/obj/Debug/net6.0/Indexer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Indexer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350d107a7303a01a64dec249668c8c027f586d98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fadc0136c1176dbd06e8a7a20f2524e856301d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indexer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Indexer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
